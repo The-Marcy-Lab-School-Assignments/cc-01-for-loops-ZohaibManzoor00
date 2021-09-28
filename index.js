4e3c29d0b947f2cec69ece094ccb67082a41379f
Zohaib <zohaibmanzoor1700@gmail.com>
2021-09-28 14:52:06 +0000

    completed some questions

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 //1
 function countToFifty(){
+  for (let i = 1; i <= 50; i++)
+  console.log(i)
   
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+  for(let i = 0; i <= num; i++)
+  console.log(i)
 }
 
 //3
 function threeAndFiveInFifty(){
-  
+  for(let i = 1; i <= 50; i++)
+  if(i % 3 === 0 || i % 5 === 0){
+      console.log(i)
+  }
 }
 
 //4
-function threeAndFive(){
-  
+function threeAndFive(num){
+ for(let i = 1; i<= num; i++)
+ if(i % 3 === 0 || i % 5 === 0){
+     console.log(i)
+ }
+}
+//takes an array of integers and returns a new array
+//of integers with triple the value.
+
+function triple(array){
+  let tripleArray = [];
+  for(let i = 0; i <= array.length; i++){
+    tripleArray.push(array[i]*3) 
+  }
+  return tripleArray;
 }
 
-
+countToFifty()
+countFromOne(23)
+threeAndFiveInFifty()
+threeAndFive(20)
+console.log(triple([1, 2, 3]))
